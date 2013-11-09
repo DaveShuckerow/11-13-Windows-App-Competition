e2cdaec07498cbc7d55dabb57c7d9d06e5521de5
David J Shuckerow <djshuckerow@hotmail.com>
2013-11-09 16:21:44 -0600

    Unity Set up

---
+++ Project/Assets/Game/Gameboard/Hex/Hex.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hex : ScriptableObject {
+    protected Hex upHex, ulHex, dlHex, dnHex, drHex, urHex;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public Hex getUp()
+    {
+        return upHex;
+    }
+    public Hex getUl()
+    {
+        return ulHex;
+    }
+    public Hex getDl()
+    {
+        return dlHex;
+    }
+    public Hex getDn()
+    {
+        return dnHex;
+    }
+    public Hex getDr()
+    {
+        return drHex;
+    }
+    public Hex getUr()
+    {
+        return urHex;
+    }
+
+    public void setUp(Hex up)
+    {
+        upHex = up;
+        up.dnHex = this;
+        finalizeNeighbors();
+    }
+    public void setUl(Hex ul)
+    {
+        ulHex = ul;
+        ul.drHex = this;
+        finalizeNeighbors();
+    }
+    public void setDl(Hex dl)
+    {
+        dlHex = dl;
+        dl.urHex = this;
+        finalizeNeighbors();
+    }
+    public void setDn(Hex dn)
+    {
+        dnHex = dn;
+        dn.upHex = this;
+        finalizeNeighbors();
+    }
+    public void setDr(Hex dr)
+    {
+        drHex = dr;
+        dr.ulHex = this;
+        finalizeNeighbors();
+    }
+    public void setUr(Hex ur)
+    {
+        urHex = ur;
+        ur.dlHex = this;
+        finalizeNeighbors();
+    }
+    private bool finalizeNeighbors()
+    {
+        if (upHex == null || ulHex == null || dlHex == null || dnHex == null || drHex == null || urHex == null)
+            return false;
+        // UpHex:
+        upHex.drHex = urHex;
+        upHex.dlHex = ulHex;
+        // UlHex:
+        ulHex.urHex = upHex;
+        ulHex.dnHex = dlHex;
+        // DlHex:
+        dlHex.upHex = ulHex;
+        dlHex.drHex = dnHex;
+        // DnHex:
+        dnHex.ulHex = dlHex;
+        dnHex.urHex = drHex;
+        // DrHex:
+        drHex.dlHex = dnHex;
+        drHex.upHex = urHex;
+        // UrHex:
+        urHex.dnHex = drHex;
+        urHex.ulHex = upHex;
+        return true;
+    }
+}
---
+++ Project/Assets/Game/Gameboard/Hex/HexTest.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HexTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        test001HexLining();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void test001HexLining() {
+        Hex h1 = new Hex();
+        Hex h2 = new Hex();
+        Hex h3 = new Hex();
+        Hex h4 = new Hex();
+        Hex h5 = new Hex();
+        Hex h6 = new Hex();
+        Hex h7 = new Hex();
+        h1.setUp(h2);
+        h1.setUl(h3);
+        h1.setDl(h4);
+        h1.setDn(h5);
+        h1.setDr(h6);
+        h1.setUr(h7);
+        bool cond1 = h1.getUp() == h2 && h1.getUl() == h3 && h1.getDl() == h4 &&
+                h1.getDn() == h5 && h1.getDr() == h6 && h1.getUr() == h7;
+        bool cond2 = h2.getDn() == h1 && h3.getDr() == h1 && h4.getUr() == h1 &&
+                h5.getUp() == h1 && h6.getUl() == h1 && h7.getDl() == h1;
+        print("Test 1: Assignment");
+        DebugUtil.Assert(cond1);
+        print("Test 2: Reflexivity");
+        DebugUtil.Assert(cond2);
+        print("Test 3: Completeness");
+        DebugUtil.Assert(h1.getUl() == h2.getDl() && h1.getUr() == h2.getDr());
+        print("Test 3-1: passed");
+        DebugUtil.Assert(h1.getDl() == h3.getDn() && h1.getUp() == h3.getUr());
+        print("Test 3-2: passed");
+        DebugUtil.Assert(h1.getDn() == h4.getDr() && h1.getUl() == h4.getUp());
+        print("Test 3-3: passed");
+        DebugUtil.Assert(h1.getDr() == h5.getUr() && h1.getDl() == h5.getUl());
+        print("Test 3-4: passed");
+        DebugUtil.Assert(h1.getUr() == h6.getUp() && h1.getDn() == h6.getDl());
+        print("Test 3-5: passed");
+        DebugUtil.Assert(h1.getUp() == h7.getUl() && h1.getDr() == h7.getDn());
+        print("Test 3-6: passed");
+    }
+}
