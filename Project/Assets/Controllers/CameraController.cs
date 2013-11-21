88db0bc85f7bf265b28794c6915454b5c4f6203a
David J Shuckerow <djshuckerow@hotmail.com>
2013-11-20 22:17:59 -0600

    Camera motion and zooming implemented

---
+++ Project/Assets/Controllers/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public float multiplier = 1;
+    public int minZoom = 100;
+    public int maxZoom = 10;
+    public float zoomAmount = 5;
+    private Vector3 prevMousePosition;
+
+	// Use this for initialization
+	void Start () {
+        prevMousePosition = Input.mousePosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    /* move with mouse */
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 newPos = (prevMousePosition - Input.mousePosition)*Time.deltaTime*multiplier;
+            transform.position += Vector3.forward*newPos.y;
+            transform.position += Vector3.right * newPos.x;
+        }
+        prevMousePosition = Input.mousePosition;
+        /* Zoom with mousewheel */
+        Camera.main.orthographicSize = Mathf.Min(Mathf.Max(Camera.main.orthographicSize-Input.GetAxis("Mouse ScrollWheel")*zoomAmount, maxZoom), minZoom);
+	}
+}
